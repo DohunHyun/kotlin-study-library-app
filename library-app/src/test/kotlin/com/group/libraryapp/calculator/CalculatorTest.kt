e5f8730b8693d831faf2b9ad36b3762ab97c1d3a
DohunHyun <dohun.hyun@gmail.com>
2024-10-24 16:06:18 +0900

    removed cached

--- library-app/src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ library-app/src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,4 +1,93 @@
 package com.group.libraryapp.calculator
 
+fun main() {
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideZeroTest()
+}
+
 class CalculatorTest {
+
+    fun addTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.add(3)
+
+        // number가 private라서 가져올수 없다. -> data class(Calculator)를 사용하는 방법.
+//        val expectedCalculator = Calculator(8)
+//        if(calculator != expectedCalculator) {
+//            throw IllegalArgumentException()
+//        }
+
+        // number를 public 으로 변경
+        // _number를 써서 getter를 만듬
+
+        // then
+        if(calculator.number != 8) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(2)
+
+        // then
+        if(calculator.number != 3) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(2)
+
+        // then
+        if(calculator.number != 10) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        if(calculator.number != 2) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun divideZeroTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            if(e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalStateException("에러 메세지가 다릅니다.")
+            }
+            // 테스트 성공
+            return
+        } catch (e: Exception) {
+            throw IllegalStateException()
+        }
+        throw IllegalStateException("기대하는 예외가 발생하지 않았습니다.")
+    }
 }
